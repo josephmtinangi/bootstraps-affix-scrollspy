491f4ff0495a0aa4cfabb458b82f32292d636175
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-02-24 21:13:01 +0300

    Add Bootstrap and JQuery cdn

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -5,8 +5,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <head>
 	<meta charset="utf-8">
 	<title>Bootstrap Affix and Scrollspy</title>
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 </head>
 <body>
 	<h1>Bootstrap Affix and Scrollspy</h1>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </body>
 </html>
