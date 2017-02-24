695fdeb6593f2cfe7f551a70e78af92ada39953b
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-02-24 21:43:08 +0300

    Implement affix and add more content

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -9,9 +9,41 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<title>Bootstrap Affix and Scrollspy</title>
 	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<style>
+		.affix {
+		  top: 20px;
+		  width: 213px;
+		}
+
+		@media (min-width: 1200px) {
+		  .affix {
+		    width: 263px;
+		  }         
+		}	
+		.affix-bottom {
+		  position: absolute;
+		  width: 213px;
+		}
+
+		@media (min-width: 1200px) {
+		  .affix-bottom {
+		    width: 263px;
+		  }
+		}			
+	</style>
 </head>
 <body>
 	
+	<div class="jumbotron">
+		<div class="container">
+			<h1>Hello, world!</h1>
+			<p>Contents ...</p>
+			<p>
+				<a class="btn btn-primary btn-lg">Learn more</a>
+			</p>
+		</div>
+	</div>
+
 	<div class="col-md-3 scrollspy">
 		<ul id="nav" class="nav hidden-xs hidden-sm" data-spy="affix">
 			<li>
@@ -42,6 +74,27 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			<p>
 				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi rerum sint asperiores natus, aliquid qui reprehenderit, praesentium consequatur ducimus perspiciatis iure quam odio voluptas facilis provident doloribus. Nesciunt mollitia, minima.
 			</p>
+
+			<h3>Intro</h3>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio voluptatum dignissimos voluptates, vel architecto ea quis ut quaerat, aperiam temporibus fugiat excepturi qui dolore possimus laudantium illum at soluta consequatur, necessitatibus vitae non. Quas dignissimos totam libero natus itaque labore, consequatur omnis reprehenderit nobis recusandae fugit, dolores consequuntur adipisci, officiis!
+			</p>
+			<h3>Fundamentals</h3>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto recusandae iure optio odio, quas aperiam aut officia explicabo. Molestiae ipsa, explicabo voluptates obcaecati rem, atque ut non, est minima aperiam et laborum dolor veritatis cumque quae similique debitis, repellendus harum sunt unde soluta recusandae perferendis? Excepturi quasi quia magni, mollitia.
+			</p>
+			<h3>Main</h3>
+			<p>
+				Silent faded sign shrine skyscraper systema smart-tube pen modem beef noodles futurity. Corporation nano-woman kanji Shibuya singularity shrine plastic assassin cardboard carbon crypto-construct sunglasses stimulate garage. Warehouse receding marketing realism table assassin singularity bicycle post-meta-physical towards carbon A.I. refrigerator dolphin ablative. 
+			</p>
+			<p>
+				Man math-network human ablative decay drone boy apophenia spook assault convenience store dolphin. Geodesic film long-chain hydrocarbons engine knife modem semiotics savant alcohol industrial grade uplink denim pre-network tiger-team. Hotdog receding beef noodles nano-paranoid network computer cartel Legba urban shrine geodesic woman courier digital realism. 
+			</p>
+			<h3>Conclusion</h3>
+			<p>
+				Motion range-rover rebar construct otaku katana footage DIY silent 8-bit numinous wristwatch artisanal BASE jump. Jeans media systema papier-mache youtube industrial grade tattoo smart-sign narrative uplink grenade chrome. Receding military-grade tattoo saturation point car knife rifle computer semiotics wonton soup concrete render-farm corrupted skyscraper denim. 
+			</p>
+
 		</section>
 		<section id="web-development">
 			<section id="ruby">
@@ -49,12 +102,58 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 				<p>
 					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae, nisi! Quae reprehenderit ratione aut incidunt porro, aliquam iusto saepe distinctio nam. Sed aliquid repudiandae nesciunt, iste maxime assumenda atque pariatur.
 				</p>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi rerum sint asperiores natus, aliquid qui reprehenderit, praesentium consequatur ducimus perspiciatis iure quam odio voluptas facilis provident doloribus. Nesciunt mollitia, minima.
+			</p>
+
+			<h3>Intro</h3>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio voluptatum dignissimos voluptates, vel architecto ea quis ut quaerat, aperiam temporibus fugiat excepturi qui dolore possimus laudantium illum at soluta consequatur, necessitatibus vitae non. Quas dignissimos totam libero natus itaque labore, consequatur omnis reprehenderit nobis recusandae fugit, dolores consequuntur adipisci, officiis!
+			</p>
+			<h3>Fundamentals</h3>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto recusandae iure optio odio, quas aperiam aut officia explicabo. Molestiae ipsa, explicabo voluptates obcaecati rem, atque ut non, est minima aperiam et laborum dolor veritatis cumque quae similique debitis, repellendus harum sunt unde soluta recusandae perferendis? Excepturi quasi quia magni, mollitia.
+			</p>
+			<h3>Main</h3>
+			<p>
+				Silent faded sign shrine skyscraper systema smart-tube pen modem beef noodles futurity. Corporation nano-woman kanji Shibuya singularity shrine plastic assassin cardboard carbon crypto-construct sunglasses stimulate garage. Warehouse receding marketing realism table assassin singularity bicycle post-meta-physical towards carbon A.I. refrigerator dolphin ablative. 
+			</p>
+			<p>
+				Man math-network human ablative decay drone boy apophenia spook assault convenience store dolphin. Geodesic film long-chain hydrocarbons engine knife modem semiotics savant alcohol industrial grade uplink denim pre-network tiger-team. Hotdog receding beef noodles nano-paranoid network computer cartel Legba urban shrine geodesic woman courier digital realism. 
+			</p>
+			<h3>Conclusion</h3>
+			<p>
+				Motion range-rover rebar construct otaku katana footage DIY silent 8-bit numinous wristwatch artisanal BASE jump. Jeans media systema papier-mache youtube industrial grade tattoo smart-sign narrative uplink grenade chrome. Receding military-grade tattoo saturation point car knife rifle computer semiotics wonton soup concrete render-farm corrupted skyscraper denim. 
+			</p>				
 			</section>
 			<section id="python">
 				<h2>Python</h2>
 				<p>
 					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima, placeat, tempore quam asperiores id maiores? Iure, ipsum quas accusamus quos iste sed illum nam molestiae beatae ducimus officiis, est porro?
 				</p>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi rerum sint asperiores natus, aliquid qui reprehenderit, praesentium consequatur ducimus perspiciatis iure quam odio voluptas facilis provident doloribus. Nesciunt mollitia, minima.
+			</p>
+
+			<h3>Intro</h3>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio voluptatum dignissimos voluptates, vel architecto ea quis ut quaerat, aperiam temporibus fugiat excepturi qui dolore possimus laudantium illum at soluta consequatur, necessitatibus vitae non. Quas dignissimos totam libero natus itaque labore, consequatur omnis reprehenderit nobis recusandae fugit, dolores consequuntur adipisci, officiis!
+			</p>
+			<h3>Fundamentals</h3>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto recusandae iure optio odio, quas aperiam aut officia explicabo. Molestiae ipsa, explicabo voluptates obcaecati rem, atque ut non, est minima aperiam et laborum dolor veritatis cumque quae similique debitis, repellendus harum sunt unde soluta recusandae perferendis? Excepturi quasi quia magni, mollitia.
+			</p>
+			<h3>Main</h3>
+			<p>
+				Silent faded sign shrine skyscraper systema smart-tube pen modem beef noodles futurity. Corporation nano-woman kanji Shibuya singularity shrine plastic assassin cardboard carbon crypto-construct sunglasses stimulate garage. Warehouse receding marketing realism table assassin singularity bicycle post-meta-physical towards carbon A.I. refrigerator dolphin ablative. 
+			</p>
+			<p>
+				Man math-network human ablative decay drone boy apophenia spook assault convenience store dolphin. Geodesic film long-chain hydrocarbons engine knife modem semiotics savant alcohol industrial grade uplink denim pre-network tiger-team. Hotdog receding beef noodles nano-paranoid network computer cartel Legba urban shrine geodesic woman courier digital realism. 
+			</p>
+			<h3>Conclusion</h3>
+			<p>
+				Motion range-rover rebar construct otaku katana footage DIY silent 8-bit numinous wristwatch artisanal BASE jump. Jeans media systema papier-mache youtube industrial grade tattoo smart-sign narrative uplink grenade chrome. Receding military-grade tattoo saturation point car knife rifle computer semiotics wonton soup concrete render-farm corrupted skyscraper denim. 
+			</p>				
 			</section>
 
 		</section>
@@ -67,7 +166,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		    offset: {
 		        top: $('#nav').offset().top
 		    }
-		});		
+		});	
+		$('#nav').affix({
+		  offset: {
+		    bottom: ($('footer').outerHeight(true) + 
+		            $('.application').outerHeight(true)) + 
+		            40
+		  }
+		});			
 	</script>
 </body>
 </html>
