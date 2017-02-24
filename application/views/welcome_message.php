91aa0c46c8ce5e13293fffe4cedbfa4d6fb55ad1
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-02-24 21:31:26 +0300

    Add mobile responsiveness

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -3,7 +3,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Bootstrap Affix and Scrollspy</title>
 	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
